5fbe0a1bd79918cce570c07d1577e3a28d578b96
Beverly Liu <bevliu@gmail.com>
2019-05-29 20:57:31 -0700

    WebDriver for Safari

---
+++ src/Selenium/SafariTest.java
@@ -0,0 +1,16 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class SafariTest {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new SafariDriver();
+		driver.get("https://www.apple.com/");
+		
+		System.out.println(driver.getTitle());
+	}
+
+}
